37510324cf94c1d01b8cf21aa89260f6b2052acb
Michael W Brock <mbrocksworld@gmail.com>
2014-07-17 22:36:19 -0400

    Update library_system_simulation_v_2.py

--- library_system_simulation_v_2.py
+++ library_system_simulation_v_2.py
@@ -1,14 +1,11 @@
 # Michael Brock © June 28, 2014
 # Summer Internship @ St. Petersburg College (SPC)
-# Library System Simulation Program in Python - Version 1
+# Library System Simulation Program in Python - Version 2
 
 ################################################################################
-# NOTE: Version 1 is one of the first Python programs that I ever wrote, and I #
-# was still learning the syntax and different types of for loop constructs. It #
-# does work, but it didn't quite satisfy the assignment requirements (i.e.,    #
-# using nested or two-dimensional lists), so I created a Version 2 that does.  #
-# I only included Version 1 here to show that there is another way to do it    #
-# using multiple one-dimensional lists.                                        #
+# NOTE: Version 2 was made to satisfy the assignment requirements of using a   #
+# "list of lists" (a.k.a. "nested list" or "two-dimensional list"). Also, I    #
+# cleaned up the code and used different logic control structures herein.        #
 ################################################################################
 
 # This program simulates a library system with library card number validation,
@@ -24,7 +21,7 @@ import sys # Required for sys.exit() function. (NOT in textbook!)
 ISBN_SEARCH = 1
 TITLE_SEARCH = 2
 QUIT_CHOICE = 3
-
+    
 ################################################################################
 # NOTE: Normally, this type of program would be accessing the data from either #
 # databases or dictionaries. But since the students working on this exercise   #
@@ -32,26 +29,29 @@ QUIT_CHOICE = 3
 # wrote this program with that in mind.                                        #
 ################################################################################
 
-# Create a global two-dimensional list with 3 rows and 2 columns, where rows
-# represent individual books and columns represent titles and 10-digit ISBNs.
-# This list is used for testing purposes only, and the data would normally be
-# obtained from a database or dictionary. I made it global so that it could be
-# used by all of the functions described in the assignment.
-books = [['Animal Farm', '0452284244'], ['Brave New World', '0060850523'], ['Catch-22', '0684833395']]
+# Create a global two-dimensional list with 3 rows and 3 columns, where rows
+# represent individual books and columns represent titles, 10-digit ISBNs and
+# a 'y' or 'n' character for availability. This list is used for testing purposes
+# only, and the data would normally be obtained from a database or dictionary. I
+# made it global so that it could be used by all of the functions described in
+# the assignment.
+books = [
+    ['Animal Farm', '0452284244', 'N'],
+    ['Brave New World', '0060850523', 'Y'],
+    ['Catch-22', '0684833395', 'Y']
+    ]
 
-################################################################################
-# NOTE: I tried using a nested list, as shown above, but I discovered that the #
-# in operator doesn't work as expected in this case, which I confirmed online  #
-# at: http://kracekumar.com/post/22512660850/python-in-operator-use-cases      #
-# So I had to resolve to making separate lists of the book titles and ISBNs,   #
-# with their indexes matching accordingly.                                     #
-################################################################################
-books_title = ['Animal Farm', 'Brave New World', 'Catch-22']
-books_isbn = ['0452284244', '0060850523', '0684833395']
-books_available = ['0452284244', '0060850523', '0684833395']
+# Create global constants for ROWS and COLS to be used in functions.
+ROWS = len(books)
+COLS = len(books)
 
 # The main function.
 def main():
+
+    # Create a variables to hold values.
+    isbn = 0
+    title = ''
+    
     # Call the getNumber function to get the library card number from user 
     # and assign it to the library_card_number variable.
     library_card_number = getNumber()
@@ -75,20 +75,62 @@ def main():
         # Create a logic control structure to handle user choices
         # and to perform the selected action.
         if choice == ISBN_SEARCH:
+            
             # Get user input for book title.
             title = input('\nEnter book title: ')
             print('\nSearching for ',title,'. Please wait...', sep='')
 
             # Call the lookUpISBN function.
-            lookUpISBN(title)
+            isbn = lookUpISBN(title)
+
+            if isbn != 0:
+                print('\nHooray, that book was found in our database and its ISBN is: ',isbn,'.', sep='')
+
+                # Is the book currently available?
+                print('\nChecking to see if the book is currently available. Please wait...')
+
+                # Call the isBookAvailable function to check for the availability.
+                isBookAvailable(isbn)
+
+            else:
+                print('\nSorry, that book was not found in our database.')
+                print('\nMake sure you spelled the title correctly, including proper capitalization.')
+                print('If not, choose 1 from the MENU, enter the title spelled correctly and try again.')
 
         elif choice == TITLE_SEARCH:
+            
             # Get user input for book ISBN.
             isbn = input('\nEnter book ISBN: ')
             print('\nSearching for ISBN: ',isbn,'. Please wait...', sep='')
 
             # Call the lookUpISBN function.
-            lookUpTitle(isbn)
+            title = lookUpTitle(isbn)
+
+            if title != 0:
+                print('\nHooray, that book was found in our database and its title is: ',title,'.', sep='')
+
+                # Is that the correct title?
+                correct_title = input('\nIs that the correct title you are looking for? Enter \"y\" or \"Y\" for yes, or \nanything else for no: ')
+
+                if correct_title == 'y' or correct_title == 'Y':
+                    # Could've used lower() or upper() above,
+                    # but students haven't learned about it yet.
+
+                    # Is the book currently available?
+                    print('\nChecking to see if the book is currently available. Please wait...')
+
+                    # Call the isBookAvailable function to check for availability.
+                    isBookAvailable(isbn)
+
+                else:
+                    print('\nSorry, please see a librarian for help.')
+
+            # If no, then return a message, letting the user know
+            # the book title wasn't found in the library database.
+            else:
+                print('\nSorry, that book was not found in our database.')
+                print('\nMake sure you entered the ISBN correctly.')
+                print('If not, choose 2 from the MENU, enter the ISBN correctly and try again.')
 
         elif choice == QUIT_CHOICE:
             print('\nThanks for visiting the St. Petersburg Public Library! Exiting the program...')
@@ -97,7 +139,7 @@ def main():
         else:
             print('\nError: invalid selection. Please enter a valid choice.')
 
-# The main function ends here.
+# End of main function.
 
 # The display_menu function displays a menu that
 # allows users to search for a book by ISBN or
@@ -111,8 +153,10 @@ def display_menu():
 # The getNumber function prompts the user to enter
 # their library card number and checks to make sure
 # it's within the range of the high and low numbers.
-# Then it returns the user's number. (PREDEFINED)
+# Then it returns the user's number.
+# (PREDEFINED FUNCTION SIGNATURE)
 def getNumber():
+    
     # Get the user's library card number and validate
     # to make sure it's in the correct range.
     card_number = int(input('Enter your library card number: '))
@@ -127,105 +171,75 @@ def getNumber():
 
 # The getCharacter function prompts the user for a
 # character string and returns the string, casted as
-# an int in this case. (PREDEFINED)
+# an int in this case. (PREDEFINED FUNCTION SIGNATURE)
 def getCharacter():
     string = int(input('\nEnter your choice: '))
     return string
 
 # The lookUpISBN function accepts a book title and
 # returns the corresponding ISBN, or 0 if the book
-# cannot be found. (PREDEFINED)
-def lookUpISBN(book_title):
+# cannot be found. (PREDEFINED FUNCTION SIGNATURE)
+def lookUpISBN(title):
         
     # Create a variables to hold values.
-    index = 0
-    isbn = ''
+    isbn = 0
 
     # Process the list, checking to see if user's book title is found.
-    # If yes, then return ISBN. If no, then return 0 and/or a message,
-    # letting the user know the book title wasn't found.
-    
-    # If Yes, then return book ISBN and check for availability.
-    if book_title in books_title:
-        # Since the index method raises a ValueError exception if the
-        # item is not found in the list, a try/except statement is used.
-        try:
-            index = books_title.index(book_title)
-            isbn = books_isbn[index]
-            print('\nHooray, that book was found in our database and its ISBN is: ',isbn,'.', sep='')
-        except ValueError:
-            print('\nSorry, that book was not found in our database.')
-
-        # Is the book currently available?
-        print('\nChecking to see if the book is currently available. Please wait...')
-		
-	# Call the isBookAvailable function to check for the availability.
-        isBookAvailable(isbn)
-        
-    # If no, then return 0 and/or a message, letting the user know
-    # the book title wasn't found.
-    else:
-        print('\nSorry, that book was not found in our database.')
-        print('\nMake sure you spelled the title correctly, including proper capitalization.')
-        print('If not, choose 1 from the MENU, enter the title spelled correctly and try again.')
+    # If yes, then return ISBN. If no, then return 0.	
+    for r in range(ROWS):
+        for c in range(COLS):
+            # If Yes, then return book ISBN.
+            if title == books[r][c]:
+                isbn = books[r][1]
+                return isbn
+    # If no, then return 0.
+    return 0
 
 # The lookUpTitle function accepts a book ISBN and
-# returns the corresponding Title, or none and/or
-# a message value if the book cannot be found.
-# (PREDEFINED)
+# returns the corresponding title, or 0 if the
+# book cannot be found. (PREDEFINED FUNCTION SIGNATURE)
 def lookUpTitle(isbn):
 
     # Create variables to hold values.
-    index = 0
     title = ''
 
     # Process the list, checking to see if user's book title is found.
-    # If yes, then return ISBN and check for availability. If no, then
-    # return 0 and/or a message, letting the user know the book title
-	# wasn't found in the library database.
-    
-    # If Yes, then return book ISBN and check for availability.
-    if isbn in books_isbn:
-        # Since the index method raises a ValueError exception if the
-        # item is not found in the list, a try/except statement is used.
-        try:
-            index = books_isbn.index(isbn)
-            title = books_title[index]
-            print('\nHooray, that book was found in our database and its title is: ',title,'.', sep='')
-        except ValueError:
-            print('\nSorry, that book was not found in our database.')
-
-        # Is that the correct title?
-        correct_title = input('\nIs that the correct title you are looking for? Enter \"y\" or \"Y\" for yes or \nanything else for no: ')
-
-        if correct_title == 'y' or correct_title == 'Y':
-            # Could've used lower() or upper() above,
-            # but students haven't learned about it yet.
-            
-            # Is the book currently available?
-            print('\nChecking to see if the book is currently available. Please wait...')
-	    # Call the isBookAvailable function to check for availability.
-            isBookAvailable(isbn)
-        else:
-            print('\nSorry, please see a librarian for help.')
-        
-    # If no, then return 0 and/or a message, letting the user know
-    # the book title wasn't found in the library database.
-    else:
-        print('\nSorry, that book was not found in our database.')
-        print('\nMake sure you entered the ISBN correctly.')
-        print('If not, choose 2 from the MENU, enter the ISBN correctly and try again.')
+    # If yes, then return book title. If no, then return 0.    
+    for r in range(ROWS):
+        for c in range(COLS):
+            # If Yes, then return book title.
+            if isbn == books[r][1]:
+                title = books[r][0]
+                return title
+    # If no, then return 0.
+    return 0
 
 # The isBookAvailable function accepts an ISBN,
 # searches the library database (a list), and
 # returns a "Y" or "N" indicating whether the
-# book is currently available. (PREDEFINED)
+# book is currently available.
+# (PREDEFINED FUNCTION SIGNATURE)
 def isBookAvailable(isbn):
-	# If Yes, then return book ISBN.
-    if isbn in books_available:
+    
+    # Create variables to hold values.
+    available = ''
+
+    # Process the list, checking to see if user's book is available.
+    # If yes, then return 'Y' and a message indicating that it is 
+    # currently available. If no, then return 'N' and/or a message
+    # indicating that it is not currently available.
+    for r in range(ROWS):
+        for c in range(COLS):
+            # If the ISBN is found, then check for availability.
+            if isbn == books[r][1]:
+                available = books[r][2]
+
+    # If yes, then return 'Y' and/or a message indicating that
+    # it is currently available.
+    if available.upper() == 'Y':
         print('\nYes, that book is currently available.')
-    # If no, then return 0 and/or a message, letting the user know
-    # the book title wasn't found in the library database.
+    # If no, then return 'N' and/or a message indicating that
+    # it is not currently available.
     else:
         print('\nNo, that book is not currently available.')
 
