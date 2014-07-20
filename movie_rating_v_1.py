87a0333c33986df8e9f3f550eb9305c259830521
Michael W Brock <mbrocksworld@gmail.com>
2014-07-19 23:36:03 -0400

    Update movie_rating_v_1.py

--- movie_rating_v_1.py
+++ movie_rating_v_1.py
@@ -1,66 +1,75 @@
 # Copyright © 2014 by Michael Brock
 # Hollywood Movie Rating Program - Version 1
-# Simple Solution
+# Simple Solution 1 with Running Total and NO Calculated Average
 
 # Version 1 is a simple program that demonstrates the use of loops and logic
-# control structures to keep a running total in Python. It gives a user 3
-# attempts to enter a valid star rating for the featured movie and displays
-# an error message after a third failed attempt, exiting the program. Otherwise,
-# it displays a message indicating what the user entered has been noted.
+# control structures to keep a running total in Python. It gives a user three
+# attempts to enter a valid star rating for the featured movie, then it either
+# displays an error message after a third failed attempt, exiting the program,
+# or displays a success message, indicating what the user entered has been
+# noted and exits the program.
 
 ###########################################################################
-# NOTE: This program didn't quite satisfy the assignment requirements, so #
-# I wrote a Version 3 that does, which is more advanced in nature.        #
+# NOTE: Versions 1 & 2 didn't quite satisfy the assignment requirements,  #
+# so I wrote a Version 3 that does, which is more advanced in nature.     #
 ###########################################################################
 
 import sys # Required for sys.exit function. (NOT in textbook!)
 
-# Global constant for the max number of tries.
+# Create a global constant for the maximum number of tries.
+
+###########################################################################
+# NOTE: In this case, I made it global simply to make it easier to change #
+# later, if necessary, because it's at the top of the program.            #
+###########################################################################
+
 MAX_TRIES = 3
 
-# Define the main function
+# Define the main function.
 def main():
 
-    # Create variables to hold numeric values and initialize to zero.
-    running_total = 0
-    rating =  0
-    average = 0
-
-    # Display an introduction message to the user (OPTIONAL).
+    # Display a descriptive introductory message to the user (OPTIONAL).
     print('This program gives a user 3 attempts to enter a valid star rating for ')
     print('the featured movie. It will display an error message and exit the ')
     print('program after a third failed attempt. Otherwise, it will display a ')
     print('message indicating what the user entered for the star rating was noted.\n ')
 
-    # Get initial input from user.
+    # Get initial input from the user.
     rating = float(input('Enter a star rating between 0 and 4: '))
 
-    # Create an accumulator variable for the number of tries in the input
-    # validation loop and initialize it to 1 to acount for the initial try from
-    # the first entry. If you set it to 0, it will not work properly and gives
-    # the user 4 tries instead of 3 as instructed.
+    # Create an accumulator variable to keep a running total for the number of
+    # tries in the input validation loop and initialize it to 1 to account for
+    # the initial try from the first entry. If you set it to 0, it will not work
+    # properly and gives the user four tries instead of three as instructed.
     tries = 1
 
-    # Validate user input
+    # Validate user input: While rating is out of range, display a helpful error
+    # message and prompt the user to enter a valid number until maximum number
+    # of tries is reached, then either exit the program immediately or display a
+    # success message and exit the program.
     while rating < 0 or rating > 4:
-        print('ERROR: Invalid input. Please enter a number between 0 and 4. ')
-        # Get input from user again.
+        # Display a helpful error message.
+        print('ERROR: Invalid input. Valid numbers range between 0 and 4 (inclusive). ')
+        # Get input from the user again.
         rating = float(input('Enter a star rating between 0 and 4: '))
         tries += 1
-
-        # Print error message and exit the program if maximum tries reached.
+        # If maximum number of tries is reached, then display an error message
+        # and exit the program.
         if rating < 0 or rating > 4 and tries >= MAX_TRIES:
             print("SORRY! you have reached the maximum number of tries. Exiting the program...")
-            sys.exit()
+            sys.exit(0)
+        # Else if rating is within range, then display a success message...
         elif rating >= 0 and rating <= 4:
             print('\nYou entered a star rating of',format(rating, '.2f'),'for the featured movie.')
-            print('Thanks, it has been noted.')
+            print('Thanks for the rating, it has been noted. Exiting the program...')
+            sys.exit(0)
 
-    # Print message indicating the star rating has been noted
-    # if the user rating is valid and max tries not reached
+    # If the user rating is initially valid, then display a success message and
+    # exit the program.
     if rating >= 0 and rating <= 4 and tries < MAX_TRIES:
         print('\nYou entered a star rating of',format(rating, '.2f'),'for the featured movie.')
-        print('Thanks, it has been noted.')
+        print('Thanks for the rating, it has been noted. Exiting the program...')
+        sys.exit(0)
      
 # Call main function to run the program.
 main()
